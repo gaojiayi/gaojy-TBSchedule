35b145ce396dea8f136ce7d3c55a1864d6118942
gaojiayi <775670823@qq.com>
2017-08-29 22:04:39 +0800

    modified:   src/main/java/com/hpe/pamirs/schedule/hpeschedule/strategy/TBScheduleManagerFactory.java 	modified:   src/main/java/com/hpe/pamirs/schedule/hpeschedule/zk/ScheduleStrategyDataManager4ZK.java

--- src/main/java/com/hpe/pamirs/schedule/hpeschedule/strategy/TBScheduleManagerFactory.java
+++ src/main/java/com/hpe/pamirs/schedule/hpeschedule/strategy/TBScheduleManagerFactory.java
@@ -1,6 +1,13 @@
 package com.hpe.pamirs.schedule.hpeschedule.strategy;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -9,44 +16,139 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+import com.hpe.pamirs.schedule.hpeschedule.ScheduleUtil;
 import com.hpe.pamirs.schedule.hpeschedule.taskmanager.IScheduleDataManager;
+import com.hpe.pamirs.schedule.hpeschedule.zk.ScheduleStrategyDataManager4ZK;
 import com.hpe.pamirs.schedule.hpeschedule.zk.ZKManager;
+
 /**
  * Title: 调度服务器构造器
+ * 使用springcontext来初始化配置
  * @author gaojy
  *
- * Create Date :2017年8月21日
- * Update Date :
+ *         Create Date :2017年8月21日 Update Date :
  */
-public class TBScheduleManagerFactory implements ApplicationContextAware{
+public class TBScheduleManagerFactory implements ApplicationContextAware {
 
-	private static transient Log logger = LogFactory.getLog(TBScheduleManagerFactory.class);
-	private Map<String ,String> zkConfig;
+	private static transient Log logger = LogFactory
+			.getLog(TBScheduleManagerFactory.class);
+	private Map<String, String> zkConfig;
 	protected ZKManager zkManager;
-	
+
 	/**
 	 * 是否启动调度管理，如果只是做系统管理，应该设置为false
 	 */
 	public boolean start = true;
-	
-	private int timeInterval = 2000;
-	
-	/**
-	 * ManagerFactoryTimerTask上次执行的时间戳</br>
-	 * zk环境不稳定，可能导致所有task自循环丢失，调度停止</br>
-	 * 外层应用，通过jmx暴露心跳时间，监控这个tbschedule最重要的大循环</br>
-	 */
+
+	private int timerInterval = 2000;
+
 	public volatile long timerTaskHeartBeatTS = System.currentTimeMillis();
-	
+
 	/**
 	 * 调度配置中心客户端
 	 */
 	private IScheduleDataManager scheduleDataManager;
 	private ScheduleStrategyDataManager4ZK scheduleStrategyManager;
 	
-	public void setApplicationContext(ApplicationContext arg0) throws BeansException {
-		// TODO Auto-generated method stub
+	private Map<String,List<IStrategyTask>> managerMap = new ConcurrentHashMap<String, List<IStrategyTask>>();
+
+	private ApplicationContext applicationcontext;
+	private String uuid;
+	private String ip;
+	private String hostName;
+	
+	private Timer timer;
+	
+
+	/**
+	 * ManagerFactoryTimerTask上次执行的时间戳</br> zk环境不稳定，可能导致所有task自循环丢失，调度停止</br>
+	 * 外层应用，通过jmx暴露心跳时间，监控这个tbschedule最重要的大循环</br>
+	 */
+	private ManagerFactoryTimerTask timerTask;
+	
+	protected Lock lock = new ReentrantLock();
+	
+	volatile String errorMesage 
+				= "No config Zookeeper connect infomation";
+	private InitialThread initialThread;
+	
+	public TBScheduleManagerFactory(){
+		this.ip = ScheduleUtil.getLocalIP();
+		this.hostName = ScheduleUtil.getLocalHostName();
+	}
+	
+	public void init() throws Exception{
+		Properties p = new Properties();
+		for(Map.Entry<String, String> e:this.zkConfig.entrySet()){
+			p.put(e.getKey(),e.getValue());
+		}
+		this.init(p);
+	}
+	
+	/**
+	 * 初始化 实现构造器的功能  实例化数据
+	 * @param p
+	 * @throws Exception
+	 */
+	public void init(Properties p) throws Exception{
+		if(this.initialThread != null){
+			this.initialThread.stopThread();
+		}
+		this.lock.lock();
+		try{
+			
+		}finally{
+			this.lock.unlock();
+		}
+	}
+	
+	public void setApplicationContext(ApplicationContext arg0)
+			throws BeansException {
 		
+
 	}
 
 }
+
+/**
+ * 定时调度任务
+ * @author gaojy
+ *
+ */
+class ManagerFactoryTimerTask extends TimerTask{
+
+	private static transient Log log
+			= LogFactory.getLog(ManagerFactoryTimerTask.class);
+	TBScheduleManagerFactory factory;
+	int count = 0;
+	
+	public ManagerFactoryTimerTask(TBScheduleManagerFactory aFactory){
+		this.factory = aFactory;
+	}
+	@Override
+	public void run() {
+		try {
+			Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+	
+}
+
+
+class InitialThread extends Thread{
+	private static transient Log log 
+	        = LogFactory.getLog(InitialThread.class);
+	TBScheduleManagerFactory factory ;
+	boolean isStop = false;
+	
+	public InitialThread(TBScheduleManagerFactory aFactory){
+		this.factory = aFactory;
+	}
+	public void stopThread(){
+		this.isStop = true;
+	}
+}
+
--- src/main/java/com/hpe/pamirs/schedule/hpeschedule/zk/ScheduleStrategyDataManager4ZK.java
+++ src/main/java/com/hpe/pamirs/schedule/hpeschedule/zk/ScheduleStrategyDataManager4ZK.java
@@ -1,26 +1,23 @@
 package com.hpe.pamirs.schedule.hpeschedule.zk;
 
-import java.lang.reflect.Type;
+
 import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.ZooKeeper;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
+import com.hpe.pamirs.schedule.hpeschedule.strategy.ScheduleStrategy;
+import com.hpe.pamirs.schedule.hpeschedule.strategy.TBScheduleManagerFactory;
+import com.hpe.pamirs.schedule.hpeschedule.zk.TimestampTypeAdapter;
 
 /**
  * 与ScheduleDataManager4ZK，最好设计成继承一个抽象类 ，该类负责创建gson，zkManager等基础属性
+ * 
  * @Title :zk 调度策略操作
  * @author gaojy
  *
@@ -29,36 +26,110 @@ import com.google.gson.JsonSerializer;
  */
 public class ScheduleStrategyDataManager4ZK {
 
-  private ZKManager zkManager;
-  private String PATH_Strategy;
-  private String PATH_ManagerFactory;
-  private Gson gson;
-  
-  //在spring对象创建完毕后，创建内部对象
-  public ScheduleStrategyDataManager4ZK(ZKManager aZKManager) throws Exception{
-    this.zkManager = aZKManager;
-    gson = new GsonBuilder()
-      .registerTypeAdapter(Timestamp.class, new TimestampTypeAdapter())
-      .setDateFormat("yyyy-MM-dd HH:mm:ss").create();
-    this.PATH_Strategy = this.zkManager.getRootPath() + "/strategy";
-    this.PATH_ManagerFactory = this.zkManager.getRootPath() + "/factory";
-    
-    if(this.getZooKeeper().exists(this.PATH_Strategy, false) == null){
-      ZKTools.createPath(getZooKeeper(), this.PATH_Strategy, CreateMode.PERSISTENT, this.zkManager.getAcl());
-    }
-    
-    if(this.getZooKeeper().exists(this.PATH_ManagerFactory, false) == null){
-     ZKTools.createPath(getZooKeeper(), PATH_ManagerFactory, CreateMode.PERSISTENT, this.zkManager.getAcl());
-    }
-  }
-  
-  
-  public ZooKeeper getZooKeeper() throws Exception {
-    return this.zkManager.getZookeeper();
-}
-public String getRootPath(){
-    return this.zkManager.getRootPath();
-}
-}
+	private ZKManager zkManager;
+	private String PATH_Strategy;
+	private String PATH_ManagerFactory;
+	private Gson gson;
 
+	// 在spring对象创建完毕后，创建内部对象
+	public ScheduleStrategyDataManager4ZK(ZKManager aZKManager)
+			throws Exception {
+		this.zkManager = aZKManager;
+		gson = new GsonBuilder()
+				.registerTypeAdapter(Timestamp.class,
+						new TimestampTypeAdapter())
+				.setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+		this.PATH_Strategy = this.zkManager.getRootPath() + "/strategy";
+		this.PATH_ManagerFactory = this.zkManager.getRootPath() + "/factory";
 
+		if (this.getZooKeeper().exists(this.PATH_Strategy, false) == null) {
+			ZKTools.createPath(getZooKeeper(), this.PATH_Strategy,
+					CreateMode.PERSISTENT, this.zkManager.getAcl());
+		}
+
+		if (this.getZooKeeper().exists(this.PATH_ManagerFactory, false) == null) {
+			ZKTools.createPath(getZooKeeper(), PATH_ManagerFactory,
+					CreateMode.PERSISTENT, this.zkManager.getAcl());
+		}
+	}
+
+	public ScheduleStrategy loadStrategy(String strategyName) throws Exception{
+		String zkPath = this.PATH_Strategy + "/" + strategyName;
+		if(this.getZooKeeper().exists(zkPath, false) == null){
+			return null;
+		}
+		String valueString = new String(this.getZooKeeper().getData(zkPath, false, null));
+		ScheduleStrategy result = this.gson.fromJson(valueString, ScheduleStrategy.class);
+		return result;
+	}
+	
+	public void createScheduleStrategy(ScheduleStrategy scheduleStrategy)
+	         throws Exception{
+		String zkPath = this.PATH_Strategy + "/"
+	                + scheduleStrategy.getStrategyName();
+		String valueString = this.gson.toJson(scheduleStrategy);
+		if(this.getZooKeeper().exists(zkPath, false) == null){
+			this.getZooKeeper().create(zkPath, valueString.getBytes(), this.zkManager.getAcl(), CreateMode.PERSISTENT);
+		}else{
+			throw new Exception("调度策略" 
+					+ scheduleStrategy.getStrategyName()
+					+ "已经存在，如果确认需要重建，请先调用deleteMachineStrategy(String taskType)删除");
+		}
+	}
+	
+	public void updateScheduleStrategy(ScheduleStrategy scheduleStrategy)throws Exception{
+		String zkPath = this.PATH_Strategy + "/" +scheduleStrategy.getStrategyName();
+		String valueString = this.gson.toJson(scheduleStrategy);
+		if (this.getZooKeeper().exists(zkPath, false) == null) {
+			this.getZooKeeper().create(zkPath, valueString.getBytes(),
+					this.zkManager.getAcl(), CreateMode.PERSISTENT);
+		} else {
+			this.getZooKeeper().setData(zkPath, valueString.getBytes(), -1);
+		}
+	}
+	
+	public void deleteMachineStrategy(String taskType) throws Exception {
+		deleteMachineStrategy(taskType, false);
+	}
+	
+	public void deleteMachineStrategy(String taskType ,boolean isForce) throws Exception{
+		String zkPath = this.PATH_Strategy + "/" + taskType;
+		if(isForce == false
+				&& this.getZooKeeper().getChildren(zkPath, null).size() > 0){
+			throw new Exception("不能删除" + taskType
+					+ "的运行策略，会导致必须重启整个应用才能停止失去控制的调度进程。"
+					+ "可以先清空IP地址，等所有的调度器都停止后再删除调度策略");
+		}
+		ZKTools.deleteTree(getZooKeeper(), zkPath);
+	}
+
+	/**
+	 * 加载所有的调度策略
+	 * @return
+	 * @throws Exception
+	 */
+	public List<ScheduleStrategy> loadAllScheduleStrategy() throws Exception{
+		String zkPath = this.PATH_Strategy;
+		List<ScheduleStrategy> result = new ArrayList<ScheduleStrategy>();
+		List<String> names = this.getZooKeeper().getChildren(zkPath, false);
+		Collections.sort(names);
+		for(String name : names){
+			result.add(this.loadStrategy(name));
+		}
+		return result;
+		
+	}
+	
+	public List<String> registerManagerFactory(
+			TBScheduleManagerFactory managerFactory) throws Exception{
+		
+	}
+	
+	public ZooKeeper getZooKeeper() throws Exception {
+		return this.zkManager.getZookeeper();
+	}
+
+	public String getRootPath() {
+		return this.zkManager.getRootPath();
+	}
+}
