949d3cad7671cc8cc45b537d2ac0d82f578608db
Gao JiaYi <jia-yi.gao@hpe.com>
2017-08-31 18:18:12 +0800

    modified:   src/main/java/com/hpe/pamirs/schedule/hpeschedule/taskmanager/TBScheduleManagerStatic.java 	modified:   src/main/java/com/hpe/pamirs/schedule/hpeschedule/taskmanager/TBScheduleProcessorSleep.java

--- src/main/java/com/hpe/pamirs/schedule/hpeschedule/taskmanager/TBScheduleManagerStatic.java
+++ src/main/java/com/hpe/pamirs/schedule/hpeschedule/taskmanager/TBScheduleManagerStatic.java
@@ -1,134 +1,328 @@
 package com.hpe.pamirs.schedule.hpeschedule.taskmanager;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.spi.LoggerFactory;
+import org.apache.zookeeper.data.Stat;
 
 import com.hpe.pamirs.schedule.hpeschedule.TaskItemDefine;
 import com.hpe.pamirs.schedule.hpeschedule.strategy.TBScheduleManagerFactory;
-
+/**
+ * 
+ * @Title :
+ * @author gaojy
+ *
+ * @Create_Date : 2017年8月31日下午2:17:53
+ * @Update_Date :
+ */
 public class TBScheduleManagerStatic extends TBScheduleManager {
 
-	private static transient Log log = LogFactory
-			.getLog(TBScheduleManagerStatic.class);
+  private static transient Log log = LogFactory.getLog(TBScheduleManagerStatic.class);
 
-	/**
-	 * 总的任务量
-	 */
-	protected int taskItemCount = 0;
+  /**
+   * 总的任务量
+   */
+  protected int taskItemCount = 0;
 
-	protected long lastFetchVersion = -1;
+  protected long lastFetchVersion = -1;
 
-	private final Object NeedReloadTaskItemLock = new Object();
+  private final Object NeedReloadTaskItemLock = new Object();
 
-	public TBScheduleManagerStatic(TBScheduleManagerFactory aFactory,
-			String baseTaskType, String ownSign,
-			IScheduleDataManager aScheduleCenter) throws Exception {
-		super(aFactory, baseTaskType, ownSign, aScheduleCenter);
-		// TODO Auto-generated constructor stub
-	}
+  public TBScheduleManagerStatic(TBScheduleManagerFactory aFactory, String baseTaskType,
+      String ownSign, IScheduleDataManager aScheduleCenter) throws Exception {
+    super(aFactory, baseTaskType, ownSign, aScheduleCenter);
+    // TODO Auto-generated constructor stub
+  }
 
-	public void initialRunningInfo() throws Exception {
-		scheduleCenter.clearExpireScheduleServer(
-				this.currenScheduleServer.getTaskType(),
-				this.taskTypeInfo.getJudgeDeadInterval());
-		List<String> list = scheduleCenter
-				.loadScheduleServerNames(this.currenScheduleServer
-						.getTaskType());
-		if (scheduleCenter.isLeader(this.currenScheduleServer.getUuid(), list)) {
-			// 是第一次启动，先清楚所有的垃圾数据
-			log.debug(this.currenScheduleServer.getUuid() + ":" + list.size());
-			this.scheduleCenter.initialRunningInfo4Static(
-					this.currenScheduleServer.getBaseTaskType(),
-					this.currenScheduleServer.getOwnSign(),
-					this.currenScheduleServer.getUuid());
-		}
-	}
+  public void initialRunningInfo() throws Exception {
+    scheduleCenter.clearExpireScheduleServer(this.currenScheduleServer.getTaskType(),
+        this.taskTypeInfo.getJudgeDeadInterval());
+    List<String> list =
+        scheduleCenter.loadScheduleServerNames(this.currenScheduleServer.getTaskType());
+    if (scheduleCenter.isLeader(this.currenScheduleServer.getUuid(), list)) {
+      // 是第一次启动，先清楚所有的垃圾数据
+      log.debug(this.currenScheduleServer.getUuid() + ":" + list.size());
+      this.scheduleCenter.initialRunningInfo4Static(this.currenScheduleServer.getBaseTaskType(),
+          this.currenScheduleServer.getOwnSign(), this.currenScheduleServer.getUuid());
+    }
+  }
 
-	@Override
-	public void initial() throws Exception {
-		new Thread(this.currenScheduleServer.getTaskType() + "_"
-				+ this.currenScheduleServer.toString() + "-StartProcess") {
-			public void run() {
-				try {
-					log.info("开始获取调度任务队列....of"
-							+ currenScheduleServer.getUuid());
+  @Override
+  public void initial() throws Exception {
+    new Thread(this.currenScheduleServer.getTaskType() + "_" + this.currenScheduleServer.toString()
+        + "-StartProcess") {
+      public void run() {
+        try {
+          log.info("开始获取调度任务队列....of" + currenScheduleServer.getUuid());
 
-					while (!isRuntimeInfoInitial) {
-						if (isStopSchedule == true) {
-							log.debug("外部命令终止调度,退出调度队列获取："
-									+ currenScheduleServer.getUuid());
-							return;
-						}
+          while (!isRuntimeInfoInitial) {
+            if (isStopSchedule == true) {
+              log.debug("外部命令终止调度,退出调度队列获取：" + currenScheduleServer.getUuid());
+              return;
+            }
 
-						try {
-							initialRunningInfo();
-							//检查是否初始化成功
-							isRuntimeInfoInitial = scheduleCenter
-									.isInitialRunningInfoSucuss(
-											currenScheduleServer
-													.getBaseTaskType(),
-											currenScheduleServer.getOwnSign());
-						} catch (Exception e) {
-							//忽略初始化的异常
-	 				    	  log.error(e.getMessage(),e);
-						}
-						 if(isRuntimeInfoInitial == false){
-							 //还没有初始化完成 等待1秒
-	    				      Thread.currentThread().sleep(1000);
-						  }
-					}
+            try {
+              initialRunningInfo();
+              // 检查是否初始化成功
+              isRuntimeInfoInitial =
+                  scheduleCenter.isInitialRunningInfoSucuss(currenScheduleServer.getBaseTaskType(),
+                      currenScheduleServer.getOwnSign());
+            } catch (Exception e) {
+              // 忽略初始化的异常
+              log.error(e.getMessage(), e);
+            }
+            if (isRuntimeInfoInitial == false) {
+              // 还没有初始化完成 等待1秒
+              Thread.currentThread().sleep(1000);
+            }
+          }
 
-					int count = 0;
-					lastReloadTaskItemListTime = scheduleCenter.getSystemTime();
-					while(getCu){
-						
-					}
-				} catch (Exception e) {
-					// TODO: handle exception
-				}
-			}
-		}.start();
+          int count = 0;
+          lastReloadTaskItemListTime = scheduleCenter.getSystemTime();
+          while (getCurrentScheduleTaskItemListNow().size() <= 0) {
 
-	}
+            if(isStopSchedule){
+              log.debug("外部命令终止调度，退出调度队列获取"+ currenScheduleServer.getUuid());
+              return;
+            }
+            Thread.currentThread().sleep(1000);
+            count = count + 1;
+          }
+          
+          String tmpStr = "TaskItemDefine:";
+          for(int i=0;i < currentTaskItemList.size();i++){
+            if(i > 0){
+              tmpStr = tmpStr  + ",";
+            }
+            tmpStr = tmpStr + currentTaskItemList.get(i);
+          }
+          log.info("获取到任务处理队列，开始调度：" + tmpStr + " of " + currenScheduleServer.getUuid());
+       
+          //任务总量
+          taskItemCount = scheduleCenter.loadAllTaskItem(currenScheduleServer.getTaskType()).size();
 
-	/**
-	 * 定时向数据配置中心更新当前服务器的心跳信息
-	 * 如果发现本次更新的时间如果已经超过了，服务器死亡的心跳时间，则不能向服务器更新信息
-	 * 而应该当做新的服务器，进行重新注册
-	 */
-	@Override
-	public void refreshScheduleServerInfo() throws Exception {
-		try {
-			rewriteScheduleInfo();
-			//如果任务信息没有初始化成功，不做任务相关的处理
-			if(this.isRuntimeInfoInitial == false){
-				return ;
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
+          //只有在已经获取到任务处理队列后才开始启动任务处理器
+          computerStart();
+        } catch (Exception e) {
+          log.error(e.getMessage(),e);
+          String str = e.getMessage();
+          if(str.length() > 300){
+            str = str.substring(0,300);
+        }
+        startErrorinfo = "启动处理异常：" + str;
+        }
+      }
+    }.start();
 
-	}
+  }
 
-	@Override
-	public void assignScheduleTask() throws Exception {
-		// TODO Auto-generated method stub
+  /**
+   * 定时向数据配置中心更新当前服务器的心跳信息 如果发现本次更新的时间如果已经超过了，服务器死亡的心跳时间，则不能向服务器更新信息 而应该当做新的服务器，进行重新注册
+   */
+  @Override
+  public void refreshScheduleServerInfo() throws Exception {
+    try {
+      rewriteScheduleInfo();
+      // 如果任务信息没有初始化成功，不做任务相关的处理
+      if (this.isRuntimeInfoInitial == false) {
+        return;
+      }
 
-	}
+      // 重新分配任务
+      this.assignScheduleTask();
 
-	@Override
-	public List<TaskItemDefine> getCurrentScheduleTaskItemList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+      // 判断是否需要重新加载任务队列，避免任务处理不必要的检查和等待
+      boolean tmpBoolean = this.isNeedReloadTaskItemList();
+      if (tmpBoolean != this.isNeedReloadtaskItem) {
+        // 只要不相同，就设置需要重新装载
+        // 因为心跳异常的时候，做了清除队列的事情，
+        // 恢复后需要重新装载
+        synchronized (NeedReloadTaskItemLock) {
+          this.isNeedReloadtaskItem = true;
+        }
+        rewriteScheduleInfo();
+      }
 
-	@Override
-	public int gettaskItemCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+      if (this.isPauseSchedule == true || this.processor != null && processor.isSleeping() == true) {
+        // 如果服务已经暂停，则需要重新定时更新cur_server 和req_server
+        // 如果服务没有暂停，一定不能调用
+        this.getCurrentScheduleTaskItemListNow();
+      }
 
+    } catch (Exception e) {
+      // 清除内存中所有已经取得的数据和任务队列，避免心跳线程失败时候导致的数据重复
+      this.clearMenoInfo();
+      if (e instanceof Exception) {
+        throw (Exception) e;
+      } else {
+        throw new Exception(e.getMessage(), e);
+      }
+    }
+
+  }
+
+  /**
+   * 版本兼容判断 在leader重新分配任务，在每个server释放原来占有的任务项时，会修改这个版本号
+   * 
+   * @return
+   * @throws Exception
+   */
+  public boolean isNeedReloadTaskItemList() throws Exception {
+    return this.lastFetchVersion < this.scheduleCenter
+        .getReloadTaskItemFlag(this.currenScheduleServer.getTaskType());
+  }
+
+  /**
+   * 判断某个任务对应的线程组是否处于僵尸状态 true 表示有线程组处于僵尸状态 需要告警
+   * 
+   * @param type
+   * @param statMap
+   * @return
+   * @throws Exception
+   */
+  private boolean isExistZombieServ(String type, Map<String, Stat> statMap) throws Exception {
+    boolean exist = false;
+    for (String key : statMap.keySet()) {
+      Stat s = statMap.get(key);
+      if (this.scheduleCenter.getSystemTime() - s.getMtime() > this.taskTypeInfo.getHeartBeatRate() * 40) {
+        log.error("zombie serverList exists! serv=" + key + ",type=" + type + "超过40次心跳周期未更新");
+        exist = true;
+      }
+    }
+
+    return exist;
+  }
+
+  /**
+   * 根据当前调度服务器的信息，重新计算分配所有的调度任务 任务的分配是需要加锁，避免数据分片错误。 为了避免数据锁带来的负面作用，通过版本号来达到锁的目的
+   * 
+   * 1.获取任务状态的版本号 2.获取所有服务器注册信息和任务队列信息 3.清除已经超过心跳周期的服务器注册信息 4.重新计算任务分配 5.更新任务状态的版本号【乐观锁】
+   * 6.更新任务队列的分配信息
+   */
+  @Override
+  public void assignScheduleTask() throws Exception {
+    scheduleCenter.clearExpireScheduleServer(this.currenScheduleServer.getTaskType(),
+        this.taskTypeInfo.getJudgeDeadInterval());
+    List<String> serverList =
+        scheduleCenter.loadScheduleServerNames(this.currenScheduleServer.getTaskType());
+
+    if (scheduleCenter.isLeader(this.currenScheduleServer.getUuid(), serverList) == false) {
+      if (log.isDebugEnabled()) {
+        log.debug(this.currenScheduleServer.getUuid() + ":不是负责任务分配的leader，直接返回");
+      }
+      return;
+    }
+
+    // 设置初始化成功的标准，避免在leader装换的时候，新增的线程组初始化失败
+    scheduleCenter.setInitialRunningInfoSucuss(this.currenScheduleServer.getBaseTaskType(),
+        this.currenScheduleServer.getTaskType(), this.currenScheduleServer.getUuid());
+    // 返回清理taskitem的cur_server个数
+    scheduleCenter.clearTaskItem(this.currenScheduleServer.getTaskType(), serverList);
+    scheduleCenter.assignTaskItem(this.currenScheduleServer.getTaskType(),
+        this.currenScheduleServer.getUuid(), this.taskTypeInfo.getMaxTaskItemsOfOneThreadGroup(),
+        serverList);
+  }
+
+  /**
+   * 重新加载当前服务器的任务队列 1.释放当前服务器持有，但是其他服务器进行申请的任务队列 2.重新获取当前服务器的处理队列
+   * 
+   * 为了避免此操作的过度，阻塞真正的数据处理能力。系统设置一个重新转载的频率，列如1分钟。
+   * 
+   * 特别注意： 此方法的调用必须是在当前所有的任务都处理完毕后才能调用，否则是否任务队列后可能数据被重复处理
+   */
+  public List<TaskItemDefine> getCurrentScheduleTaskItemList() {
+    try {
+      if (this.isNeedReloadtaskItem == true) {
+        // 特别需要注意：需要判断数据队列是否已经空了，否则可能在队列切换的时候导致数据重复处理
+        if (this.processor != null) {
+          while (this.processor.isDealFinishAllData() == false) {
+            Thread.sleep(50);
+          }
+
+        }
+        // 真正开始处理数据
+        synchronized (NeedReloadTaskItemLock) {
+          this.getCurrentScheduleTaskItemListNow();
+          this.isNeedReloadtaskItem = false;
+        }
+      }
+      this.lastReloadTaskItemListTime = this.scheduleCenter.getSystemTime();
+      return this.currentTaskItemList;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public int gettaskItemCount() {
+    return this.taskItemCount;
+  }
+
+  // 由于上面的数据执行时有使用到Synchronized 但是心跳线程并没有对应加锁
+  // 所以在此方法上加一下synchronized 20151015
+  protected synchronized List<TaskItemDefine> getCurrentScheduleTaskItemListNow() throws Exception {
+    // 如果已经稳定了，理论上不需要加载去扫描所有的叶子节点
+    try {
+      Map<String, Stat> statMap =
+          this.scheduleCenter.getCurrentServerStatList(this.currenScheduleServer.getTaskType());
+
+      // server 下面的机器节点的运行时环境是否在刷新
+      isExistZombieServ(this.currenScheduleServer.getTaskType(), statMap);
+    } catch (Exception e) {
+      log.error("zombie serverList exists， Exception:", e);
+    }
+
+    // 获取最新的版本号
+    this.lastFetchVersion =
+        this.scheduleCenter.getReloadTaskItemFlag(this.currenScheduleServer.getTaskType());
+    log.debug("this.currenScheduleServer.getTaskType()=" + this.currenScheduleServer.getTaskType()
+        + ", need reload=" + isNeedReloadtaskItem);
+
+    try {
+      // 是否被人申请的队列 即req_server是否存在数据 其他server请求
+      this.scheduleCenter.releaseDealTaskItem(this.currenScheduleServer.getTaskType(),
+          this.currenScheduleServer.getUuid());
+
+      // 重新查询当前服务器能够处理的队列
+      // 为了避免在休眠切换的过程中出现队列瞬间不一致，先清除内存中的队列
+      this.currentTaskItemList.clear();
+      // 此处加载了任务队列
+      this.currentTaskItemList =
+          this.scheduleCenter.reloadDealTaskItem(this.currenScheduleServer.getTaskType(),
+              this.currenScheduleServer.getUuid());
+
+      // 如果超过20个心跳周期还没有获取到调度队列，则报警
+      if (this.currentTaskItemList.size() == 0
+          && scheduleCenter.getSystemTime() - this.lastReloadTaskItemListTime > this.taskTypeInfo
+              .getHeartBeatRate() * 20) {
+        StringBuffer buf = new StringBuffer();
+        buf.append("调度服务器");
+        buf.append(this.currenScheduleServer.getUuid());
+        buf.append("[TASK_TYPE=");
+        buf.append(this.currenScheduleServer.getTaskType());
+        buf.append("]自启动以来，超过20个心跳周期，还 没有获取到分配的任务队列;");
+        buf.append("  currentTaskItemList.size() =" + currentTaskItemList.size());
+        buf.append(" ,scheduleCenter.getSystemTime()=" + scheduleCenter.getSystemTime());
+        buf.append(" ,lastReloadTaskItemListTime=" + lastReloadTaskItemListTime);
+        buf.append(" ,taskTypeInfo.getHeartBeatRate()=" + taskTypeInfo.getHeartBeatRate() * 10);
+        log.warn(buf.toString());
+      }
+
+      if (this.currentTaskItemList.size() > 0) {
+        // 更新时间戳
+        this.lastReloadTaskItemListTime = scheduleCenter.getSystemTime();
+      }
+      return this.currentTaskItemList;
+    } catch (Exception e) {
+      this.lastFetchVersion = -1; // 必须把版本号设置-1，避免任务加载失败
+      if (e instanceof Exception) {
+        throw (Exception) e;
+      } else {
+        throw new Exception(e);
+      }
+    }
+  }
 }
--- src/main/java/com/hpe/pamirs/schedule/hpeschedule/taskmanager/TBScheduleProcessorSleep.java
+++ src/main/java/com/hpe/pamirs/schedule/hpeschedule/taskmanager/TBScheduleProcessorSleep.java
@@ -1,5 +1,67 @@
 package com.hpe.pamirs.schedule.hpeschedule.taskmanager;
 
-public class TBScheduleProcessorSleep {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.hpe.pamirs.schedule.hpeschedule.IScheduleTaskDeal;
+
+/**
+ * 任务调度器,在TBScheduleManager的管理下实现多线程数据处理
+ * @Title :
+ * @author gaojy
+ *
+ * @Create_Date : 2017年8月31日下午2:25:35
+ * @Update_Date :
+ */
+public class TBScheduleProcessorSleep<T> implements IScheduleProcessor,Runnable {
+
+  private static transient Log logger = LogFactory.getLog(TBScheduleProcessorSleep.class);
+  
+  final LockObject m_lockObject = new LockObject();
+  
+  List<Thread> threadList = new CopyOnWriteArrayList<Thread>();
+  
+  /**
+   * 任务管理器
+   */
+  protected TBScheduleManager scheduleManager;
+  
+  /**
+   * 任务类型
+   */
+  ScheduleTaskType taskTypeInfo;
+  /**
+   * 任务处理的接口类
+   */
+  protected IScheduleTaskDeal<T> taskDealBean;
+ 
+  protected long taskListVersion
+  
+  public void run() {
+    
+  }
+
+  public boolean isDealFinishAllData() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  public boolean isSleeping() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  public void stopSchedule() throws Exception {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public void clearAllHasFetchData() {
+    // TODO Auto-generated method stub
+    
+  }
 
 }
